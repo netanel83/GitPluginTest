9300d123abd57c53675353a6fa13895da8e38fa3
NETANEL <netanel83@hotmail.com>
2016-09-28 10:56:31 +0300

    adding printing

--- src/gitTest/TestClass.java
+++ src/gitTest/TestClass.java
@@ -2,8 +2,7 @@ package gitTest;
 
 public class TestClass {
 	public static void main (String args[]){
-		System.out.println("changed");
-		int n = 6;
-		n +=2;
+		System.out.println("in bug #212 checking here");
+
 	}
 }
